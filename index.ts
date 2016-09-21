3196f3fbe412e859c343224fe779213761bb9984
Thomas Waterfall <thomas.waterfall@gmail.com>
2016-09-21 19:17:26 +0100

    Changed approach slightly

--- index.ts
+++ index.ts
@@ -5,7 +5,6 @@ const Inert = require('inert');
 const Lout = require('lout');
 const Vision = require('vision');
 const Fs = require('fs');
-const Path = require('path');
 
 
 const server: Hapi.Server = new Hapi.Server();
@@ -18,9 +17,7 @@ server.register([Vision, Inert, Lout], (err) => {
     var controllerPath = "./src/controllers/";
     var controllers = Fs.readdirSync(controllerPath);
     controllers.forEach(function (controller) {
-        if (Path.extname(controllerPath + controller) === '.js') {
-            server.route(require(controllerPath + controller));
-        }
+        server.route(require(controllerPath + controller));
     });
     server.start((err) => {
         if (err) {
